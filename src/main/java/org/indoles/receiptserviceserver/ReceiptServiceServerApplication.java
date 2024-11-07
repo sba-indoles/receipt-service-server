3328fa87600e2a5b0d746972fce3e839ff332cc3
jay-so <merry9504@naver.com>
2024-11-07 13:18:25 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/org/indoles/receiptserviceserver/ReceiptServiceServerApplication.java
@@ -0,0 +1,13 @@
+package org.indoles.receiptserviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptServiceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/indoles/receiptserviceserver/ReceiptServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.indoles.receiptserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
