938bdf1e4eaf5ae95f0c3c9e36c64bb6e8269bc0
jay-so <merry9504@naver.com>
2024-11-13 09:38:31 +0900

    refactor: 거래 내역 서비스 리팩토링

--- src/main/java/org/indoles/receiptserviceserver/core/receipt/service/ReceiptService.java
+++ src/main/java/org/indoles/receiptserviceserver/core/receipt/service/ReceiptService.java
@@ -2,9 +2,19 @@ package org.indoles.receiptserviceserver.core.receipt.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.indoles.receiptserviceserver.core.member.dto.SignInInfo;
+import org.indoles.receiptserviceserver.core.receipt.domain.Receipt;
+import org.indoles.receiptserviceserver.core.receipt.dto.*;
 import org.indoles.receiptserviceserver.core.receipt.infra.ReceiptRepository;
+import org.indoles.receiptserviceserver.global.exception.AuthorizationException;
+import org.indoles.receiptserviceserver.global.exception.ErrorCode;
+import org.indoles.receiptserviceserver.global.exception.NotFoundException;
+import org.indoles.receiptserviceserver.global.util.Mapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
@@ -13,30 +23,54 @@ public class ReceiptService {
 
     private final ReceiptRepository receiptRepository;
 
-//    public ReceiptInfo getReceiptInfo(SignInInfo memberInfo, UUID receiptId) {
-//        Receipt receipt = receiptRepository.findById(receiptId)
-//                .orElseThrow(() -> new NotFoundException("거래 내역을 찾을 수 없습니다. id=" + receiptId, ErrorCode.R000));
-//
-//        if (!receipt.isOwnedBy(memberInfo.id())) {
-//            throw new AuthorizationException("자신이 판매한 거래 내역 혹은 구매한 거래 내역만 조회할 수 있습니다.", ErrorCode.R001);
-//        }
-//
-//        return Mapper.convertToReceiptInfo(receipt);
-//    }
+    /**
+     * 거래 내역 상세 조회 서비스
+     *
+     * @param memberInfo
+     * @param receiptId
+     * @return
+     */
 
-//    public List<BuyerReceiptSimpleInfo> getBuyerReceiptSimpleInfos(SignInInfo buyerInfo,
-//                                                                   BuyerReceiptSearchCondition condition) {
-//        List<Receipt> receipts = receiptRepository.findAllByBuyerId(buyerInfo.id(), condition);
-//        return receipts.stream()
-//                .map(Mapper::convertToBuyerReceiptSimpleInfo)
-//                .toList();
-//    }
-//
-//    public List<SellerReceiptSimpleInfo> getSellerReceiptSimpleInfos(SignInInfo sellerInfo,
-//                                                                     SellerReceiptSearchCondition condition) {
-//        List<Receipt> receipts = receiptRepository.findAllBySellerId(sellerInfo.id(), condition);
-//        return receipts.stream()
-//                .map(Mapper::convertToSellerReceiptSimpleInfo)
-//                .toList();
-//    }
+    public ReceiptInfo getReceiptInfo(SignInInfo memberInfo, UUID receiptId) {
+        Receipt receipt = receiptRepository.findById(receiptId)
+                .orElseThrow(() -> new NotFoundException("거래 내역을 찾을 수 없습니다. id=" + receiptId, ErrorCode.R000));
+
+        if (!receipt.isOwnedBy(memberInfo.id())) {
+            throw new AuthorizationException("자신이 판매한 거래 내역 혹은 구매한 거래 내역만 조회할 수 있습니다.", ErrorCode.R001);
+        }
+
+        return Mapper.convertToReceiptInfo(receipt);
+    }
+
+    /**
+     * 거래 내역 조회 서비스(구매자 전용)
+     *
+     * @param buyerInfo
+     * @param condition
+     * @return
+     */
+
+    public List<BuyerReceiptSimpleInfo> getBuyerReceiptSimpleInfos(SignInInfo buyerInfo,
+                                                                   BuyerReceiptSearchCondition condition) {
+        List<Receipt> receipts = receiptRepository.findAllByBuyerId(buyerInfo.id(), condition);
+        return receipts.stream()
+                .map(Mapper::convertToBuyerReceiptSimpleInfo)
+                .toList();
+    }
+
+    /**
+     * 거래 내역 조회 서비스(판매자 전용)
+     *
+     * @param sellerInfo
+     * @param condition
+     * @return
+     */
+
+    public List<SellerReceiptSimpleInfo> getSellerReceiptSimpleInfos(SignInInfo sellerInfo,
+                                                                     SellerReceiptSearchCondition condition) {
+        List<Receipt> receipts = receiptRepository.findAllBySellerId(sellerInfo.id(), condition);
+        return receipts.stream()
+                .map(Mapper::convertToSellerReceiptSimpleInfo)
+                .toList();
+    }
 }
