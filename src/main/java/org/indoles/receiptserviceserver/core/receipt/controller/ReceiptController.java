1922cc0973f6d50e36afa3bf82b908a64843647d
jay-so <merry9504@naver.com>
2024-11-13 09:17:54 +0900

    refator: ReceiptController 리팩토링

--- src/main/java/org/indoles/receiptserviceserver/core/receipt/controller/ReceiptController.java
+++ src/main/java/org/indoles/receiptserviceserver/core/receipt/controller/ReceiptController.java
@@ -2,8 +2,19 @@ package org.indoles.receiptserviceserver.core.receipt.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.indoles.receiptserviceserver.core.member.controller.BuyerOnly;
+import org.indoles.receiptserviceserver.core.member.controller.Login;
+import org.indoles.receiptserviceserver.core.member.controller.Roles;
+import org.indoles.receiptserviceserver.core.member.controller.SellerOnly;
+import org.indoles.receiptserviceserver.core.member.domain.Role;
+import org.indoles.receiptserviceserver.core.member.dto.SignInInfo;
+import org.indoles.receiptserviceserver.core.receipt.dto.*;
 import org.indoles.receiptserviceserver.core.receipt.service.ReceiptService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/receipts")
@@ -11,35 +22,52 @@ import org.springframework.web.bind.annotation.*;
 public class ReceiptController {
 
     private final ReceiptService receiptService;
-//
-//    // 구매자는 자신의 거래 이력 목록을 조회할 수 있다.
-//    @BuyerOnly
-//    @GetMapping("/buyer")
-//    public ResponseEntity<List<BuyerReceiptSimpleInfo>> getReceipts(@Login SignInInfo buyerInfo,
-//                                                                    @RequestParam(name = "offset") int offset,
-//                                                                    @RequestParam(name = "size") int size) {
-//        BuyerReceiptSearchCondition condition = new BuyerReceiptSearchCondition(offset, size);
-//        List<BuyerReceiptSimpleInfo> infos = receiptService.getBuyerReceiptSimpleInfos(buyerInfo, condition);
-//        return ResponseEntity.ok(infos);
-//    }
-//
-//    // 판매자는 자신의 거래 이력 목록을 조회할 수 있다.
-//    @SellerOnly
-//    @GetMapping("/seller")
-//    public ResponseEntity<List<SellerReceiptSimpleInfo>> getSellerReceipts(@Login SignInInfo sellerInfo,
-//                                                                           @RequestParam(name = "offset") int offset,
-//                                                                           @RequestParam(name = "size") int size) {
-//        SellerReceiptSearchCondition condition = new SellerReceiptSearchCondition(offset, size);
-//        List<SellerReceiptSimpleInfo> infos = receiptService.getSellerReceiptSimpleInfos(sellerInfo, condition);
-//        return ResponseEntity.ok(infos);
-//    }
-//
-//    // 사용자는 자신의 거래 이력을 상세 조회할 수 있다.
-//    @Roles({Role.BUYER, Role.SELLER})
-//    @GetMapping("/{receiptId}")
-//    public ResponseEntity<ReceiptInfo> getReceipt(@Login SignInInfo memberInfo,
-//                                                  @PathVariable("receiptId") UUID receiptId) {
-//        ReceiptInfo receiptInfo = receiptService.getReceiptInfo(memberInfo, receiptId);
-//        return ResponseEntity.ok(receiptInfo);
-//    }
+
+    /**
+     * 거래 내역 조회 API(구매자 전용)
+     * @param buyerInfo
+     * @param offset
+     * @param size
+     * @return
+     */
+    @BuyerOnly
+    @GetMapping("/buyer")
+    public ResponseEntity<List<BuyerReceiptSimpleInfo>> getReceipts( SignInInfo buyerInfo,
+                                                                    @RequestParam(name = "offset") int offset,
+                                                                    @RequestParam(name = "size") int size) {
+        BuyerReceiptSearchCondition condition = new BuyerReceiptSearchCondition(offset, size);
+        List<BuyerReceiptSimpleInfo> infos = receiptService.getBuyerReceiptSimpleInfos(buyerInfo, condition);
+        return ResponseEntity.ok(infos);
+    }
+
+    /**
+     * 거래 내역 조회 API(판매자 전용)
+     * @param sellerInfo
+     * @param offset
+     * @param size
+     * @return
+     */
+    @SellerOnly
+    @GetMapping("/seller")
+    public ResponseEntity<List<SellerReceiptSimpleInfo>> getSellerReceipts( SignInInfo sellerInfo,
+                                                                           @RequestParam(name = "offset") int offset,
+                                                                           @RequestParam(name = "size") int size) {
+        SellerReceiptSearchCondition condition = new SellerReceiptSearchCondition(offset, size);
+        List<SellerReceiptSimpleInfo> infos = receiptService.getSellerReceiptSimpleInfos(sellerInfo, condition);
+        return ResponseEntity.ok(infos);
+    }
+
+    /**
+     * 거래 내역 상세 조회 API(구매자, 판매자 공통)
+     * @param memberInfo
+     * @param receiptId
+     * @return
+     */
+    @Roles({Role.BUYER, Role.SELLER})
+    @GetMapping("/{receiptId}")
+    public ResponseEntity<ReceiptInfo> getReceipt( SignInInfo memberInfo,
+                                                  @PathVariable("receiptId") UUID receiptId) {
+        ReceiptInfo receiptInfo = receiptService.getReceiptInfo(memberInfo, receiptId);
+        return ResponseEntity.ok(receiptInfo);
+    }
 }
