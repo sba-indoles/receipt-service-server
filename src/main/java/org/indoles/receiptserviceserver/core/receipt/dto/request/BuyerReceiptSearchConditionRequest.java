a0192714fb3978ff42e29685a88e624b678e26e5
jay-so <merry9504@naver.com>
2024-11-18 01:54:50 +0900

    refactor: Search 조건 리팩토링

--- src/main/java/org/indoles/receiptserviceserver/core/receipt/dto/request/BuyerReceiptSearchConditionRequest.java
+++ src/main/java/org/indoles/receiptserviceserver/core/receipt/dto/request/BuyerReceiptSearchConditionRequest.java
@@ -6,7 +6,7 @@ import static org.indoles.receiptserviceserver.core.receipt.dto.validate.Validat
 
 /**
  * 구매자가 거래 내역 목록 조회시 적용할 수 있는 조건을 나타내는 DTO
- *
+ * @param offset 조회할 거래 내역의 시작 위치 (default: 0)
  * @param size 조회할 거래 내역의 개수 (default: 10) (Min: 1, Max: 100)
  */
 public record BuyerReceiptSearchConditionRequest(
@@ -15,7 +15,7 @@ public record BuyerReceiptSearchConditionRequest(
 ) {
 
     public BuyerReceiptSearchConditionRequest {
-        validateSizeBetween(0, 100, size);
+        validateSizeBetween(1, 100, size);
         validateOffset(offset);
     }
 }
--- src/main/java/org/indoles/receiptserviceserver/core/receipt/dto/request/SellerReceiptSearchConditionRequest.java
+++ src/main/java/org/indoles/receiptserviceserver/core/receipt/dto/request/SellerReceiptSearchConditionRequest.java
@@ -6,7 +6,7 @@ import static org.indoles.receiptserviceserver.core.receipt.dto.validate.Validat
 
 /**
  * 판매자가 거래 내역 목록 조회시 적용할 수 있는 조건을 나타내는 DTO
- *
+ * @param offset 조회할 거래 내역의 시작 위치 (default: 0)
  * @param size 조회할 거래 내역의 개수 (default: 10) (Min: 1, Max: 100)
  */
 public record SellerReceiptSearchConditionRequest(
@@ -15,7 +15,7 @@ public record SellerReceiptSearchConditionRequest(
 ) {
 
     public SellerReceiptSearchConditionRequest {
-        validateSizeBetween(0, 100, size);
+        validateSizeBetween(1, 100, size);
         validateOffset(offset);
     }
 }
